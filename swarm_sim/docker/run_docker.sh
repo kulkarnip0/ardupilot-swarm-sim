43b201d457a9093caf06e5e5ff2ac12bb35fb6a6
Samarth <samvkulk@gmail.com>
2026-06-18 21:52:48 +0530

    Configure Dockerfile and run script for NVIDIA GLX hardware acceleration

--- swarm_sim/docker/run_docker.sh
+++ swarm_sim/docker/run_docker.sh
@@ -1,10 +1,13 @@
 xhost +local:docker
+xhost +local:root
+
 docker run -it --rm \
   --name ardupilot-sim-harmonic \
   --network=host \
   --hostname ardupilot-sim-harmonic \
+  --gpus all \
   -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
   -e QT_X11_NO_MITSHM=1 \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /home/praveen/workspace/shatriya:/workspace \
   ardupilot-sim-harmonic
